747111a0580bd2ab78e53c2727f6b98d5590b19d
Nicolai Brand <nicolaihbrand@gmail.com>
2022-08-15 00:00:59 +0200

    feat: parser for binary expressions

---
+++ src/main/java/dev/lytix/lox/Parser.java
@@ -0,0 +1,195 @@
+package dev.lytix.lox;
+
+import java.util.List;
+
+import static dev.lytix.lox.TokenType.*;
+
+/*
+ * recursive descent parser.
+ *
+ * complete context-free expression grammar:
+ *
+ * expression   -> equality ;
+ * equality     -> comparison ( ( "!=" | "==" ) comparison )* ;
+ * comparison   -> term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+ * term         -> factor ( ( "-" | "+" ) factor )* ;
+ * factor       -> unary ( ( "/" | "*" ) unary )* ;
+ * unary        -> ( "!" | "-") unary
+ *              |  primary;
+ * primary      -> NUMBER | STRING | "true" | "false" | "nil"
+ *              |  "(" expression ")" ;
+ */
+public class Parser {
+    private static class ParseError extends RuntimeException {}
+    private final List<Token> tokens;
+    /* points to the current token that is being parsed */
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    Expr parse() {
+        try {
+            return expression();
+        } catch (ParseError error) {
+            return null;
+        }
+    }
+
+    private Expr expression() {
+        /* expression   -> equality ; */
+        return equality();
+    }
+
+    private Expr equality() {
+        /* equality     -> comparison ( ( "!=" | "==" ) comparison )* ; */
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    /* equality */
+    private Expr comparison() {
+        /* comparison   -> term ( ( ">" | ">=" | "<" | "<=" ) term )* ; */
+        Expr expr = term();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    /* plus or minus */
+    private Expr term() {
+        /* term         -> factor ( ( "-" | "+" ) factor )* ; */
+        Expr expr = factor();
+
+        while (match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    /* multiplication or division */
+    private Expr factor() {
+        /* factor       -> unary ( ( "/" | "*" ) unary )* ; */
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr unary() {
+        /* unary        -> ( "!" | "-") unary
+                        |  primary; */
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    private Expr primary() {
+        /* primary      -> NUMBER | STRING | "true" | "false" | "nil"
+                        |  "(" expression ")" ; */
+        if (match(NUMBER, STRING))
+            return new Expr.Literal(previous().literal);
+
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            /* consume until we find closing parenthesis */
+            consume(RIGHT_PAREN, "Expected ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+
+        /* entering here means token can't start an expression */
+        throw error(peek(), "Expected expression.");
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) return advance();
+
+        throw error(peek(), message);
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        //TODO: if (current == 0) return error;
+        return tokens.get(current - 1);
+    }
+
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
+    }
+
+    private void synchronize() {
+        /* discard tokens until we are at the beginning of the next statement */
+        advance();
+
+        while (!isAtEnd()) {
+            /* semicolon signifies statement ending */
+            if (previous().type == SEMICOLON) return;
+
+            switch (peek().type) {
+                case CLASS, FUN, VAR, FOR, IF, WHILE, PRINT, RETURN:
+                    return;
+            }
+
+            advance();
+        }
+    }
+
+
+}
---
+++ src/main/java/dev/lytix/tool/GenerateAst.java
@@ -0,0 +1,103 @@
+package dev.lytix.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Binary     : Expr left, Token Operator, Expr right",
+                "Grouping   : Expr expression",
+                "Literal    : Object value",
+                "Unary      : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+       String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
+
+        writer.println("package dev.lytix.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
+        /* the Abstract Syntax Tree classes */
+        String className;
+        String fields;
+        String[] split;
+        for (String type : types) {
+            split = type.split(":");
+            className = split[0].trim();
+            fields = split[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        /* the base accept() method */
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
+        writer.println("}");
+        writer.close();
+    }
+
+    /* declare a visit method for each subclass */
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        String typeName;
+        for (String type : types) {
+            typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" + typeName +
+                    " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        String[] fields = fieldList.split(", ");
+
+        writer.println();
+        writer.println("    static class " + className + " extends " + baseName + " {");
+
+        /* fields */
+        for (String field : fields)
+            writer.println("        final " + field + ";");
+        writer.println();
+
+        /* constructor */
+        writer.println("        " + className + "(" + fieldList + ") {");
+
+        /* store parameters in fields */
+        String name;
+        for (String field : fields) {
+            name = field.split(" ")[1];
+            writer.println("            this." + name + " = " + name + ";");
+        }
+
+        writer.println("        }");
+
+        /* visitor pattern */
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
+
+        writer.println("    }");
+    }
+}
