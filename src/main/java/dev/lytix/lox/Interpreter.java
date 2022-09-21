5582fa1925928ef8ada997c3bc17e29dc2cc0e05
Nicolai Brand <nicolahb@stud.ntnu.no>
2022-09-21 15:24:41 +0200

    feat: semantic analysis (#4)

--- src/main/java/dev/lytix/lox/Interpreter.java
+++ src/main/java/dev/lytix/lox/Interpreter.java
@@ -6,12 +6,15 @@ import dev.lytix.lox.natives.ClockNative;
 import dev.lytix.lox.natives.FreadNative;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static dev.lytix.lox.TokenType.*;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     final Environment globals = new Environment();
+    private final Map<Expr, Integer> locals = new HashMap<>();
     private Environment environment = globals;
     boolean REPL = false;
 
@@ -44,6 +47,10 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     private void execute(Stmt stmt) {
         stmt.accept(this);
+    }
+
+    void resolve(Expr expr, int depth) {
+        locals.put(expr, depth);
     }
 
     void executeBlock(List<Stmt> statements, Environment environment) {
@@ -198,17 +205,28 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
-        environment.assign(expr.name, value);
+
+        Integer distance = locals.get(expr);
+        if (distance != null)
+            environment.assignAt(distance, expr.name, value);
+        else
+            globals.assign(expr.name, value);
         return value;
     }
 
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        Object res = environment.get(expr.name);
+        return lookUpVariable(expr.name, expr);
         //if (res == null)
         //    throw new RuntimeError(expr.name, "Attempting to access a variable that is not initialized");
+    }
 
-        return res;
+    private Object lookUpVariable(Token name, Expr expr) {
+        Integer distance = locals.get(expr);
+        if (distance != null)
+            return environment.getAt(distance, name.lexeme);
+        else
+            return globals.get(name);
     }
 
     @Override
